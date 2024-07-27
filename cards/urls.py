ccb6a1f767ba92667d9d30b631ef7c05b747ebe7
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-07-27 13:47:57 +0400

    lesson_51: добавили кликабельные тэги

--- cards/urls.py
+++ cards/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('catalog/', views.catalog, name='catalog'),  # Общий каталог всех карточек
     path('categories/', views.get_categories, name='categories'),  # Список всех категорий
     path('categories/<slug:slug>/', views.get_cards_by_category, name='category'),  # Карточки по категории
-    path('tags/<slug:slug>/', views.get_cards_by_tag, name='tag'),  # Карточки по тегу
+    path('tags/<int:tag_id>/', views.get_cards_by_tag, name='get_cards_by_tag'),  # Карточки по тегу
     path('<int:card_id>/detail/', views.get_detail_card_by_id, name='detail_card_by_id'),  # Детальная информация по карточке
 ]
--- cards/views.py
+++ cards/views.py
@@ -158,11 +158,20 @@ def get_cards_by_category(request, slug):
     return HttpResponse(f'Cards by category {slug}')
 
 
-def get_cards_by_tag(request, slug):
+def get_cards_by_tag(request, tag_id):
     """
     Возвращает карточки по тегу для представления в каталоге
     """
-    return HttpResponse(f'Cards by tag {slug}')
+    # добываем карточки из БД по тегу
+    cards = Card.objects.filter(tags__id=tag_id)
+
+    # подготавливаем контекст и отображаем шаблон
+    context = {
+        'cards': cards,
+        'menu': info['menu']
+    }
+
+    return render(request, 'cards/catalog.html', context)
 
 
 def get_detail_card_by_id(request, card_id):
