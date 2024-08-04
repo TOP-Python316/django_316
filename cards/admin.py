d8a98a570151ea1efbd870afe446adc93dae6d98
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-08-04 12:49:34 +0400

    lesson_54: добавили в админ. панель поле наличие кода и доп. действия

--- cards/admin.py
+++ cards/admin.py
@@ -5,7 +5,7 @@ from .models import Card
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     # поля, которые будут отображаться в админке
-    list_display = ('pk', 'question', 'category', 'views', 'upload_date', 'status')
+    list_display = ('pk', 'question', 'category', 'views', 'upload_date', 'status', 'brief_info')
     # поля, которые будут ссылками
     list_display_links = ('pk', 'upload_date',)
     # поля, по которым можно проводить поиск
@@ -15,9 +15,27 @@ class CardAdmin(admin.ModelAdmin):
     # сортировка по полям
     ordering = ('-views', 'category')
     # изменение выводимого количества элементов на страницу
-    list_per_page = 25
+    list_per_page = 12
     # поля, которые можно редактировать напрямую
     list_editable = ('views', 'question', 'status')
+
+    actions = ['set_checked', 'set_unchecked']
+
+    @admin.action(description='Пометить как проверенные')
+    def set_checked(self, request, queryset):
+        updated_count = queryset.update(status=Card.Status.CHECKED)
+        self.message_user(request, f'{updated_count} записей было помечено как проверенные')
+
+    @admin.action(description='Пометить как непроверенные')
+    def set_unchecked(self, request, queryset):
+        updated_count = queryset.update(status=Card.Status.UNCHECKED)
+        self.message_user(request, f'{updated_count} записей было помечено как непроверенные')
+
+    # определение метода для отображения краткой информации в карточке
+    @admin.display(description='Наличие кода', ordering='answer')
+    def brief_info(self, card):
+        has_code = 'Да' if '```' in card.answer else 'Нет'  # проверяем наличие кода
+        return has_code
 
 
 # сначала задаётся класс, который добавляет модель в админку
