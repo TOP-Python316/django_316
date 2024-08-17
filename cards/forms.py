503edb15d8a1f873c0b072ee96acfd38137e2a81
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-08-17 13:19:44 +0400

    lesson_55: дополнил шаблон с построчным рендером полей ввода

--- cards/forms.py
+++ cards/forms.py
@@ -1,8 +1,15 @@
 # cards/forms.py
 
 from django import forms
+from .models import Category
 
 
 class CardForm(forms.Form):
     question = forms.CharField(label='Вопрос', max_length=100)
     answer = forms.CharField(label='Ответ', widget=forms.Textarea, max_length=5000)
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        label='Категория',
+        required=False,
+        empty_label='Выберите категорию',
+    )
