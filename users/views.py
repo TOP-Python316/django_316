6d3e061f3acb0cfe6482a4ef94dab008b5ef9a91
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-09-01 13:58:28 +0500

    lesson_59: функции-представления для аутентификации и выхода из системы

--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginUserForm
 
 
 def login_user(request):
-    return HttpResponse('Вы вошли в систему!')
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('catalog')  # перенаправление на страницу каталога, после входа
+    else:
+        form = LoginUserForm()
+    return render(request, 'users/login.html', {'form': form})
 
 
 def logout_user(request):
-    return HttpResponse('Вы вышли из системы!')
+    logout(request)
+    # перенапавление на страницу входа, используя reverse для получения URL по имени
+    return redirect(reverse('users:login'))
