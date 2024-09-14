f1e4a2cf106ca22bb360ec59bcf6896c640066c2
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-09-14 12:38:23 +0500

    lesson_62: расширил модель пользователя

--- anki/settings.py
+++ anki/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-from django.conf.global_settings import AUTHENTICATION_BACKENDS
+from django.conf.global_settings import AUTHENTICATION_BACKENDS, AUTH_USER_MODEL
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -154,3 +155,9 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',  # стандартный бэкенд для аутентификации по username
     'users.authentication.EmailAuthBackend',  # дополнительный бэкенд для аутентификации по email
 ]
+
+
+AUTH_USER_MODEL = 'users.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    photo = models.ImageField(
+        upload_to='users/images/%Y/%m/%d',
+        null=True,
+        blank=True,
+        verbose_name='Аватар'
+    )
+    date_birth = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата рождения'
+    )
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        verbose_name='user permissions',
+    )
+
+    def __str__(self):
+        return self.username
