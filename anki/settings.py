c2001b448a2c7cbf478870431ac56b7831876b61
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-09-14 14:04:11 +0500

    lesson_62: восстановление пароля через email

--- anki/settings.py
+++ anki/settings.py
@@ -14,7 +14,9 @@ import os
 from pathlib import Path
 
 from django.conf.global_settings import AUTHENTICATION_BACKENDS, AUTH_USER_MODEL
+from dotenv import load_dotenv
 
+load_dotenv()  # Загружаем переменные окружения из файла .env в той же директории, что и settings.py
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p#ko7=t&+x#zv7tzn97qms7a6rc==-as639um$$-7-h4e$j(2g'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -161,3 +163,14 @@ AUTH_USER_MODEL = 'users.User'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+SERVER_EMAIL = os.getenv('EMAIL_HOST_USER')
+EMAIL_ADMIN = os.getenv('EMAIL_HOST_USER')
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy, path
+
 from . import views
 
 
@@ -9,11 +11,36 @@ urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', views.LogoutUser.as_view(), name='logout'),
     path('signup/', views.RegisterUser.as_view(), name='signup'),
+
     # Сообщение об успешной регистрации
     path('register_done/', views.RegisterDoneView.as_view(), name='register_done'),
+
     # Профиль, изменение пароля, мои карточки
     path('profile/', views.ProfileUser.as_view(), name='profile'),
     path('password_change/', views.UserPasswordChange.as_view(), name='password_change'),
     path('password_change_done/', views.UserPasswordChangeDone.as_view(), name='password_change_done'),
     path('profile_cards/', views.UserCardsView.as_view(), name='profile_cards'),
+
+    # маршрут для сброса пароля
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset_form.html',
+        email_template_name='users/password_reset_email.html',
+        success_url=reverse_lazy('users:password_reset_done'),
+    ), name='password_reset'),
+
+    # маршрут для подтверждения сброса пароля
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    # маршрут для ввода нового пароля
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html',
+        success_url=reverse_lazy('users:password_reset_complete'),
+    ), name='password_reset_confirm'),
+
+    # маршрут для завершения сброса пароля
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
